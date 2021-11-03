216bd5627a01bbf6dd6a3afd5b5d1e8475ff6c38
Matt B <mjbeezer11621@gmail.com>
2021-11-02 20:21:21 -0400

    first

---
+++ DeckOfCardsAPI/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using DeckOfCardsAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeckOfCardsAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private CardsDAL cardsDAL = new CardsDAL();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult CardsResult()
+        {
+            CardsModel result = cardsDAL.GetData();
+            return View(result);
+        }
+
+        [HttpPost]
+        public IActionResult CardsResult(string deck_id)
+        {
+            
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DeckOfCardsAPI/Models/CardsDAL.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace DeckOfCardsAPI.Models
+{
+    public class CardsDAL
+    {
+        public CardsModel GetData()
+        {
+            string key = "jes44pi8e6tp";
+            string url = $"https://deckofcardsapi.com/api/deck/{key}/draw/?count=5";
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+
+            string JSON = reader.ReadToEnd();
+
+            CardsModel result = JsonConvert.DeserializeObject<CardsModel>(JSON);
+
+            return result;
+        }
+    }
+}
